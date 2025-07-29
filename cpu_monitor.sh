3342ae2f876d37f4531a9e1fb7d1c4708140d2da
leshevi <79736739+leshevi@users.noreply.github.com>
2025-07-29 15:24:36 +0700

    Create cpu_monitor.sh

---
+++ cpu_monitor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true; do
+    clear
+    echo "🖥  CPU Load Monitor"
+    echo "----------------------"
+    mpstat 1 1 | awk '/Average/ {print "Загрузка CPU: " 100 - $NF "%"}'
+    sleep 1
+done
